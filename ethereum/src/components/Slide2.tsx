b4b38720d9ca311bf1e53753200d975465cdba7d
Luis Sanchez <luis.sanchezz.vazquezz@gmail.com>
2023-10-09 19:25:48 -0600

    fix: Fix design issue by adding a <section> tag to Slide2 component

--- ethereum/src/components/Slide2.tsx
+++ ethereum/src/components/Slide2.tsx
@@ -1,12 +1,14 @@
 export const Slide2 = () => {
 	return (
-		<div className="my-5">
-			<h2>¿Cómo adquirirlo?</h2>
-			<p>
-				La forma más sencilla y popular de adquirir Ethereum (ETH) es a través de un intercambio en línea, dado que
-				Ethereum (ETH) es la segunda criptomoneda más valorada y reconocida después de Bitcoin (BTC). Encontrar un
-				intercambio en línea no debería ser una tarea difícil.
-			</p>
-		</div>
+		<section>
+			<div>
+				<h2>¿Cómo adquirirlo?</h2>
+				<p>
+					La forma más sencilla y popular de adquirir Ethereum (ETH) es a través de un intercambio en línea, dado que
+					Ethereum (ETH) es la segunda criptomoneda más valorada y reconocida después de Bitcoin (BTC). Encontrar un
+					intercambio en línea no debería ser una tarea difícil.
+				</p>
+			</div>
+		</section>
 	)
 }
